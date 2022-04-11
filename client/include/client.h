05f89e4ffd7d9b273c2d6f9102066463c01479fe
NikitaBukhta <nikita.bukhta.dev@gmail.com>
2022-04-11 13:24:10 +0300

    Add constructor and destructor for Client class

--- client/include/client.h
+++ client/include/client.h
@@ -1,4 +1,26 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+namespace cs    // client-server
+{
+    class Client
+    {
+    private:
+        struct sockaddr_in address;
+        int serverSocket;
+
+    public:
+        Client(int port, char *IPv4);
+
+        ~Client(void);
+
+        void readMessage(char *buffer);
+
+        void sendMessage(char *message);
+    };
+}
+
 #endif // !CLIENT_H
--- client/src/client.cpp
+++ client/src/client.cpp
@@ -1,1 +1,30 @@
 #include "client.h"
+#include "posix_wrapping.h"
+
+#include <iostream>         // std::cerr
+#include <sys/socket.h>
+#include <unistd.h>         // close()
+
+cs::Client::Client(int port, char *IPv4)
+{
+    address = {0};
+    address.sin_family = AF_INET;
+    address.sin_port = htons(port);
+
+    try
+    {
+        POSIX::_inetPton(address.sin_family, IPv4, &address.sin_addr);
+        
+        serverSocket = POSIX::_socket(address.sin_family, SOCK_STREAM, IPPROTO_TCP);
+        POSIX::_connect(serverSocket, (struct sockaddr*)(&address), sizeof(address));
+    }
+    catch(const POSIX::PosixError& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+}
+
+cs::Client::~Client(void)
+{
+    close(serverSocket);
+}
